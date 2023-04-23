e46b2eacb86cb1aa8d1714ec7be89303e2793892
dvicofirn <90802825+dvicofirn@users.noreply.github.com>
2023-04-23 20:51:50 +0300

    Remove unnecessary parts & credit
    Original code credit is due to staff of technion, electronic commerce course of spring 2023, The faculty of data and decision making

--- data_source/additions/create_additons.py
+++ data_source/additions/create_additons.py
@@ -4,26 +4,9 @@ import networkx as nx
 import random
 import pandas as pd
 
-# CHANGE ################################
-influencers = [0, 348, 414, 3980, 686]
-# CHANGE ################################
-
 p = 0.01
 chic_choc_path = 'chic_choc_data.csv'
 cost_path = 'costs.csv'
-
-
-def create_graph(edges_path: str) -> nx.Graph:
-    """
-    Creates the Chic Choc social network
-    :param edges_path: A csv file that contains information obout "friendships" in the network
-    :return: The Chic Choc social network
-    """
-    edges = pd.read_csv(edges_path).to_numpy()
-    net = nx.Graph()
-    net.add_edges_from(edges)
-    return net
-
 
 def change_network(net: nx.Graph) -> nx.Graph:
     """
@@ -45,38 +28,6 @@ def change_network(net: nx.Graph) -> nx.Graph:
     return net,edges_to_add
 
 
-def buy_products(net: nx.Graph, purchased: set) -> set:
-    """
-    Gets the network at the beginning of stage t and simulates a purchase round
-    :param net: The network at stage t
-    :param purchased: All the users who bought a doll up to and including stage t-1
-    :return: All the users who bought a doll up to and including stage t
-    """
-    new_purchases = set()
-    for user in net.nodes:
-        neighborhood = set(net.neighbors(user))
-        b = len(neighborhood.intersection(purchased))
-        n = len(neighborhood)
-        prob = b / n
-        if prob >= random.uniform(0, 1):
-            new_purchases.add(user)
-
-    return new_purchases.union(purchased)
-
-
-def get_influencers_cost(cost_path: str, influencers: list) -> int:
-    """
-    Returns the cost of the influencers you chose
-    :param cost_path: A csv file containing the information about the costs
-    :param influencers: A list of your influencers
-    :return: Sum of costs
-    """
-    costs = pd.read_csv(cost_path)
-    return sum([costs[costs['user'] == influencer]['cost'].item()
-                if influencer in list(costs['user'])
-                else 0 for influencer in influencers])
-
-
 if __name__ == '__main__':
 
     edges = pd.read_csv(chic_choc_path).to_numpy()
@@ -93,5 +44,5 @@ if __name__ == '__main__':
         csvData.extend(edges_to_add)
         print("finished round", i + 1)
     set = pd.DataFrame(csvData, columns=['user', 'firend'])
-    set.to_csv("C:\\Users\\USER\\Desktop\\hw1_ec\\addition" + str(outer) + ".csv", index=False)#You should change that!
+    set.to_csv("C:\\Users\\USER\\Desktop\\hw1_ec\\additions\\addition" + str(outer) + ".csv", index=False)
 
